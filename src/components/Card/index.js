89dc155ffe3388c807f71fa2c5a00865b89761c0
Erica Andrade <erica.andradesa@gmail.com>
2021-02-12 15:36:36 -0300

    subindo projeto

---
+++ src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <p>Seja a primeira a experimentar o nosso hamburguer?</p>
+      <span>FAÇA O SEU PEDIDO AGORA MESMO!</span>
+
+      <button>Clique aqui</button>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className='logo'>
+        <h1>NÃO PERCA TEMPO</h1>
+        <h2>Não vai se arrepender</h2>
+      </div>
+
+      <nav>
+        <ul>
+          <li>Lojas</li>
+          <li>Serviços</li>
+          <li>Fale conosco</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
